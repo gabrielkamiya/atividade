6755b4f71c492279a0e7b9e773dc03e734cde9b6
Gabriel Kamiya <gabrielkamiya@hotmail.com>
2018-04-24 19:31:10 +0100

    Correções

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import * as Database from "./config/database";
 import { getConfiguracaoServidor } from './config/config';
 import * as App from "./src";
 
-//Comentario de teste git 2
+//Comentario de teste
 
 console.log(`Running enviroment ${process.env.NODE_ENV || "dev"}`);
 
